6a2c33adbcc43c390cdf149ba8a7795c9652301e
Sergey Bodnaryuk <s.bodnaryuk@gmail.com>
2022-10-04 11:51:21 +1000

    Обновляет сборку №2

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
 'use strict';
 
+var advancedVariables = require('postcss-advanced-variables');
+var autoprefixer = require('autoprefixer');
+var del = require('del');
 var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var inlineSvg = require('postcss-inline-svg');
+var minify = require('gulp-csso');
+var mixins = require('postcss-mixins');
+var nested = require('postcss-nested');
 var plumber = require('gulp-plumber');
 var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var minify = require('gulp-csso');
+var presetEnv = require('postcss-preset-env');
 var rename = require('gulp-rename');
-var imagemin = require('gulp-imagemin');
-var svgstore = require('gulp-svgstore');
-var svgmin = require('gulp-svgmin');
-var server = require('browser-sync');
-var del = require('del');
-var inlinesvg = require('postcss-inline-svg');
-var inlinesvgopt = require('postcss-svgo');
-var postcssMixins = require('postcss-mixins');
-var postcssAdvancedVariables = require('postcss-advanced-variables');
-var postcssNested = require('postcss-nested');
-var postcssPresetEnv = require('postcss-preset-env');
-var webp = require('gulp-webp');
 var sourcemaps = require('gulp-sourcemaps');
+var svgmin = require('gulp-svgmin');
+var svgo = require('postcss-svgo');
+var svgstore = require('gulp-svgstore');
+var sync = require('browser-sync').create();
+var webp = require('gulp-webp');
 
 function clean() {
   return del('build');
@@ -38,21 +38,22 @@ function copy() {
 
 function html() {
   return gulp.src('*.html')
+    .pipe(plumber())
     .pipe(gulp.dest('build'))
-    .pipe(server.reload({stream: true}));
+    .pipe(sync.stream());
 }
 
-function style() {
+function styles() {
   return gulp.src('postcss/style.css')
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(postcss([
-      postcssAdvancedVariables(),
-      postcssMixins(),
-      postcssNested(),
-      postcssPresetEnv(),
-      inlinesvg(),
-      inlinesvgopt(),
+      advancedVariables(),
+      mixins(),
+      nested(),
+      presetEnv(),
+      inlineSvg(),
+      svgo(),
       autoprefixer()
     ]))
     .pipe(gulp.dest('build/css'))
@@ -60,7 +61,14 @@ function style() {
     .pipe(rename('style.min.css'))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('build/css'))
-    .pipe(server.reload({stream: true}));
+    .pipe(sync.stream());
+}
+
+function scripts() {
+  return gulp.src('js/**/*.js')
+    .pipe(plumber())
+    .pipe(gulp.dest('build/js'))
+    .pipe(sync.stream());
 }
 
 function images() {
@@ -72,11 +80,9 @@ function images() {
         quality: 100
       }),
       imagemin.svgo({
-        plugins: [
-          {
-            removeViewBox: false
-          }
-        ]
+        plugins: [{
+          removeViewBox: false
+        }]
       })
     ]))
     .pipe(gulp.dest('build/img'));
@@ -98,22 +104,24 @@ function symbols() {
     .pipe(gulp.dest('build/img'));
 }
 
-function serve() {
-  server.init({
+function server() {
+  sync.init({
     server: 'build',
+    cors: true,
     notify: false,
     open: true,
     ui: false
   });
 
-  gulp.watch('postcss/**/*.css', style);
   gulp.watch('*.html', html);
+  gulp.watch('postcss/**/*.css', styles);
+  gulp.watch('js/**/*.js', scripts);
 }
 
 var build = gulp.series(
     clean,
     copy,
-    style,
+    styles,
     images,
     createWebp,
     symbols
@@ -122,10 +130,11 @@ var build = gulp.series(
 exports.clean = clean;
 exports.copy = copy;
 exports.html = html;
-exports.style = style;
+exports.styles = styles;
+exports.scripts = scripts;
 exports.images = images;
 exports.webp = createWebp;
 exports.symbols = symbols;
-exports.serve = serve;
+exports.server = server;
 
 exports.default = build;
